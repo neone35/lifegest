6059c3b95f551889047fe8e51e8abe8f6eff429e
Artur Maslov <aartasas@gmail.com>
2024-08-26 15:54:41 +0300

    Initial php backend main file

---
+++ backend/index.php
@@ -0,0 +1,38 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve data from POST request as json
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // Extract the data sent from the React frontend
+    $grandparentsAvgAge = $data['grandparentsAvgAge'];
+    $sleepHours = $data['sleepHours'];
+    $commuteHours = $data['commuteHours'];
+    $workHours = $data['workHours'];
+
+    // Results calculation
+    $averageLifeExpectancy = $grandparentsAvgAge;
+    $weeksInYear = 52;
+    $hoursInWeek = 168;
+    $totalWeeks = $averageLifeExpectancy * $weeksInYear;
+
+    $sleepWeeks = $sleepHours / $hoursInWeek;
+    $commuteWeeks = $commuteHours / $hoursInWeek;
+    $workWeeks = $workHours / $hoursInWeek;
+
+    $freeTimeWeeks = $totalWeeks - ($sleepWeeks + $commuteHours + $workHours);
+
+    // Create the response
+    $response = [
+        'totalWeeks' => $totalWeeks,
+        'sleepWeeks' => $sleepWeeks,
+        'commuteWeeks' => $commuteWeeks,
+        'workWeeks' => $workWeeks,
+        'freeTimeWeeks' => $freeTimeWeeks
+    ];
+
+    // Return the response in JSON format
+    echo json_encode($response);
+}
